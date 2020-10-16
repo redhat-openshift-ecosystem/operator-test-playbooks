8b030a9a8645e7a49a2bfa1a8091bcdc552c8547
Martin Vala <martin.vala@hotovo.com>
2020-10-16 08:41:07 +0200

    Testing script for init
    Signed-off-by: Martin Vala <martin.vala@hotovo.com>

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Hello"
+pip install ansible jmespath
+
+ansible-pull -U https://github.com/J0zi/operator-test-playbooks -C upstream-community -i localhost, -e ansible_connection=local -e run_upstream=true --tags base,docker,kind,kubectl
+
+kubectl get all --all-namespaces
