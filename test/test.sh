1971950be0c6567cf0fc6c4b5977d0781684e151
Martin Vala <martin.vala@hotovo.com>
2020-10-16 08:43:37 +0200

    Test2
    Signed-off-by: Martin Vala <martin.vala@hotovo.com>

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,7 @@
 
 pip install ansible jmespath
 
-ansible-pull -U https://github.com/J0zi/operator-test-playbooks -C upstream-community -i localhost, -e ansible_connection=local -e run_upstream=true --tags base,docker,kind,kubectl
+ansible-pull -U https://github.com/J0zi/operator-test-playbooks -C upstream-community -i localhost, -e ansible_connection=local -e run_upstream=true --tags $*
+#base,docker,kind,kubectl
 
 kubectl get all --all-namespaces
