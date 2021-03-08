e3d785b0b1b414da6b784f9e4163ed17b478eff3
kpavic <kpavic@redhat.com>
2021-03-08 14:20:39 +0100

    Add a playbook for parsing collected image pull events
    * Add the DPTP shell script for collecting pull events
    * Add the option to use pre-collected image pull events
    * Add the playbook for CVP usage

---
+++ roles/collect_modified_images/files/optional-operators-cvp-common-test-commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Collect all image pull events from the testing cluster
+# The output will contain the timestamp and the event message
+# in a comma-separated value format
+
+PULL_EVENTS_ART="${ARTIFACT_DIR}/image_pull_events.csv"
+echo "Collecting all image pull events from the testing cluster in ${PULL_EVENTS_ART}"
+oc get events --all-namespaces --field-selector reason==Pulling -o go-template='{{range .items}}{{.lastTimestamp}},{{.message}}{{"\n"}}{{end}}' > "${PULL_EVENTS_ART}"
