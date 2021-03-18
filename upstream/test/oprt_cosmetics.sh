c8540c52918c5e4dcaebbb2398bc01fb2aaf6ca9
J0zi <jbreza@redhat.com>
2021-03-18 18:17:38 +0100

    Rho 909 cosmetics script (#103)
    * loop csvs
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * install separated
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * parser changed
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * stream added
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * fix
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * fix2
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * fix3
    
    Signed-off-by: j0zi <jbreza@redhat.com>
    
    * fix4
    
    Signed-off-by: j0zi <jbreza@redhat.com>

---
+++ upstream/test/oprt_cosmetics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set +o pipefail
+
+GODEBUG=x509ignoreCN=0 ansible-pull -U $OP_TEST_ANSIBLE_PULL_REPO -C $OP_TEST_ANSIBLE_PULL_BRANCH -i localhost, -e ansible_connection=local upstream/local.yml -e run_upstream=true --tags cosmetics_install -e run_bundle_test=true -e run_remove_catalog_repo=false -vv -e run_prepare_catalog_repo_upstream=false
+
+
+for CSV_TO_TEST in $OP_TEST_MODIFIED_CSVS; do
+CSV_PATH_TO_TEST="https://raw.githubusercontent.com/$OP_TEST_MODIFIED_REPO_BRANCH/$CSV_TO_TEST"
+
+GODEBUG=x509ignoreCN=0 ansible-pull -U $OP_TEST_ANSIBLE_PULL_REPO -C $OP_TEST_ANSIBLE_PULL_BRANCH -i localhost, -e ansible_connection=local upstream/local.yml -e run_upstream=true --tags cosmetics -e run_bundle_test=true -e run_remove_catalog_repo=false -vv -e csv_external_file_path=$CSV_PATH_TO_TEST -e run_prepare_catalog_repo_upstream=false
+done
