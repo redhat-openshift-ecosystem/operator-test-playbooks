ed097121dcf18da0534dec6bd56c48b793a5a5bd
Samvaran kashyap rallabandi <srallaba@redhat.com>
2021-06-15 09:18:25 -0400

    Update entrypoint script to run fix_etc_passwd.sh within run_tests.py

--- Dockerfiles/midstream/fix_etc_passwd.sh
+++ Dockerfiles/midstream/fix_etc_passwd.sh
@@ -4,26 +4,23 @@ main() {
     local home
     home=$(mk_home "$username")
     setup_passwd "$username" "$home"
-    exec sleep inf
 }
 mk_home() {
     local username="${1:?}"
-    [[ -d /workDir ]] && [[ -w /workDir ]] && \
-        mkdir -p "/workDir/home/$username" && \
-        echo "/workDir/home/$username" && \
-        return 0
+    mkdir -p "/tmp/home/$username" && \
+    echo "/tmp/home/$username" && \
+    return 0
     mktemp --tmpdir -d "${username}.XXXXXX"
 }
 setup_passwd() {
     local username="${1:?}"
     local home="${2:?}"
-    local uid="$3"
+    local uid="$(id -u)"
     local gid="${4:-0}"
-    local passwd=/etc/passwd
     ! whoami 2>/dev/null || return 0
-    if ! [[ -w "$passwd" ]]; then
-        echo "There is no permission to add user $username to $passwd"
-        return 0
+    if ! [[ $(getent passwd $uid) ]]; then
+       echo "There is no permission to add user $username to $passwd"
+       return 0
     fi
     [[ "$uid" ]] || uid="$(id -u)"
     echo \
