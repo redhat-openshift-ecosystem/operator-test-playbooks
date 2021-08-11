8b8a73b988af2beb86fbc842b513e6bee126980e
Martin Vala <mavala@redhat.com>
2021-08-11 09:59:53 +0200

    Testing dev playbook image
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- upstream/playbook_version.sh
+++ upstream/playbook_version.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export VERSION="v$(date +%Y%m%d)"
-echo "VERSION=$VERSION"
+
+VERSION="v$(date +%Y%m%d)"
+if [ -n "$1" ];then
+    # VERSION="${VERSION}_${1}"
+    VERSION=$1
+    TAG=$1
+else
+    TAG="latest"
+fi
+
+echo "VERSION=${VERSION}"
 echo "::set-output name=image_version::${VERSION}"
+echo "::set-output name=image_tag::${TAG}"
