b4ae370bdc15927610f7921e65172f683caf3aee
J0zi <jbreza@redhat.com>
2021-08-18 14:54:00 +0200

    Upstream community dev (#221)
    * Testing dev playbook image
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Init version for remove
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Operator remove is able to remove one version
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Handling
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix dirname
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Test
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * push bundle with timestamp (#211)
    
    * use the task (#212)
    
    * separator (#213)
    
    * export vars (#214)
    
    * do not delete backups (#215)
    
    * do not backup kind-registry (#216)
    
    * do not backup kind-registry for ptq (#217)
    
    * Index skip implemented
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * debug deletion
    
    * next level filtering
    
    * debug
    
    * cleanup
    
    * Run tests
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * We are not producing image automatically on push
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * ignore string in local.yml
    
    * ignore string in role defaults
    
    * ignore delete
    
    * spaces
    
    Co-authored-by: Martin Vala <mavala@redhat.com>

--- upstream/playbook_version.sh
+++ upstream/playbook_version.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-export VERSION="v$(date +%Y%m%d)"
-echo "VERSION=$VERSION"
+
+VERSION="v$(date +%Y%m%d)"
+if [ "$1" = "refs/heads/upstream-community" ];then
+    TAG="latest"
+elif [ "$1" = "refs/heads/upstream-community-dev" ];then
+    TAG="latest"
+    # VERSION="${VERSION}_${1}"
+    VERSION=dev
+    TAG=dev
+else
+    echo "Not supported branch: '$1'"
+    exit 1
+fi
+
+echo "VERSION=${VERSION}"
 echo "::set-output name=image_version::${VERSION}"
+echo "::set-output name=image_tag::${TAG}"
