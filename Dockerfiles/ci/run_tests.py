8140c26f7ecd3ede2ea51885721659c903ee61fb
kpavic <kpavic@redhat.com>
2021-09-20 13:15:58 +0200

    Add deprecated bundle CI unit tests

--- Dockerfiles/ci/run_tests.py
+++ Dockerfiles/ci/run_tests.py
@@ -53,6 +53,38 @@ class RunOperatorTestPlaybookTests(unittest.TestCase):
             print(validation_rc)
             self.assertEqual(validation_rc, "1")
 
+    def test_validate_v48_operator_bundle_success(self):
+        exec_cmd = "ansible-playbook -vvv -i localhost, --connection local \
+                operator-test-playbooks/validate-operator-bundle.yml \
+                -e 'operator_dir={operator_dir}' \
+                -e 'operator_work_dir={operator_work_dir}' \
+                -e 'work_dir={work_dir}'".format(operator_dir=self.operator_dir,
+                                                 operator_work_dir=self.operator_work_dir,
+                                                 work_dir=self.work_dir)
+        subprocess.run(exec_cmd, shell=True)
+        self.assertTrue(path.exists("{}/validation-rc.txt".format(self.work_dir)))
+        self.assertTrue(path.exists("{}/validation-output.txt".format(self.work_dir)))
+        with open("{}/validation-rc.txt".format(self.work_dir), "r") as fd:
+            validation_rc = fd.read()
+            print(validation_rc)
+            self.assertEqual(validation_rc, "0")
+
+    def test_validate_v43_operator_bundle_failure(self):
+        exec_cmd = "ansible-playbook -vvv -i localhost, --connection local \
+                operator-test-playbooks/validate-operator-bundle.yml \
+                -e 'operator_dir={operator_dir}' \
+                -e 'operator_work_dir={operator_work_dir}' \
+                -e 'work_dir={work_dir}'".format(operator_dir=self.operator_dir,
+                                                 operator_work_dir=self.operator_work_dir,
+                                                 work_dir=self.work_dir)
+        subprocess.run(exec_cmd, shell=True)
+        self.assertTrue(path.exists("{}/validation-rc.txt".format(self.work_dir)))
+        self.assertTrue(path.exists("{}/validation-output.txt".format(self.work_dir)))
+        with open("{}/validation-rc.txt".format(self.work_dir), "r") as fd:
+            validation_rc = fd.read()
+            print(validation_rc)
+            self.assertEqual(validation_rc, "1")
+
 
 if __name__ == '__main__':
     test_name = os.getenv('TEST_NAME')
