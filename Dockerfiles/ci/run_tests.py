bc446200cc9d0c830fc298ee0cad084b38147020
kpavic <kpavic@redhat.com>
2021-09-20 11:06:03 +0200

    Add default positive operator bundle unit test

--- Dockerfiles/ci/run_tests.py
+++ Dockerfiles/ci/run_tests.py
@@ -18,28 +18,48 @@ class RunOperatorTestPlaybookTests(unittest.TestCase):
                                   "/project/output/")
         self.playbooks_dir = os.getenv('PLAYBOOKS_DIR',
                                        "/project/operator-test-playbooks/")
+        self.bundle_image = os.getenv('BUNDLE_IMAGE',
+                                      "quay.io/cvpops/test-default-positive-v1")
 
-    def test_for_report_success_empty_alm_examples(self):
+    def test_validate_default_operator_bundle_success(self):
         exec_cmd = "ansible-playbook -vvv -i localhost, --connection local \
-                    operator-test-playbooks/prepare-operator-metadata.yml \
-                    -e 'operator_dir={operator_dir}' \
-                    -e 'operator_work_dir={operator_work_dir}' \
-                    -e'work_dir={work_dir}'".format(operator_dir=self.operator_dir,
-                                                    operator_work_dir=self.operator_work_dir,
-                                                    work_dir=self.work_dir)
+                operator-test-playbooks/validate-operator-bundle.yml \
+                -e 'operator_dir={operator_dir}' \
+                -e 'operator_work_dir={operator_work_dir}' \
+                -e 'work_dir={work_dir}'".format(operator_dir=self.operator_dir,
+                                                 operator_work_dir=self.operator_work_dir,
+                                                 work_dir=self.work_dir)
         subprocess.run(exec_cmd, shell=True)
-        self.assertTrue(path.exists("{}/parsed_operator_data.yml".format(self.work_dir)))
-        self.assertTrue(path.exists("{}/parse_operator_metadata_results.json".format(self.playbooks_dir)))
-        with open("{}/parse_operator_metadata_results.json".format(self.playbooks_dir), "r") as fd:
-            result_output = json.load(fd)
-            print(result_output)
-            self.assertEqual(result_output["result"], "pass")
+        self.assertTrue(path.exists("{}/validation-rc.txt".format(self.work_dir)))
+        self.assertTrue(path.exists("{}/validation-output.txt".format(self.work_dir)))
+        with open("{}/validation-rc.txt".format(self.work_dir), "r") as fd:
+            validation_rc = fd.read()
+            print(validation_rc)
+            self.assertEqual(validation_rc, "0")
+
+    def test_validate_default_operator_bundle_failure(self):
+        exec_cmd = "ansible-playbook -vvv -i localhost, --connection local \
+                operator-test-playbooks/validate-operator-bundle.yml \
+                -e 'operator_dir={operator_dir}' \
+                -e 'operator_work_dir={operator_work_dir}' \
+                -e 'work_dir={work_dir}'".format(operator_dir=self.operator_dir,
+                                                 operator_work_dir=self.operator_work_dir,
+                                                 work_dir=self.work_dir)
+        subprocess.run(exec_cmd, shell=True)
+        self.assertTrue(path.exists("{}/validation-rc.txt".format(self.work_dir)))
+        self.assertTrue(path.exists("{}/validation-output.txt".format(self.work_dir)))
+        with open("{}/validation-rc.txt".format(self.work_dir), "r") as fd:
+            validation_rc = fd.read()
+            print(validation_rc)
+            self.assertEqual(validation_rc, "1")
 
 
 if __name__ == '__main__':
-    test_name = os.getenv('TEST_NAME', "test_for_report_success_empty_alm_examples")
-    suite = unittest.TestSuite()
-    suite.addTest(RunOperatorTestPlaybookTests(test_name))
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    test_name = os.getenv('TEST_NAME')
+    if test_name:
+        suite = unittest.TestSuite()
+        suite.addTest(RunOperatorTestPlaybookTests(test_name))
+        runner = unittest.TextTestRunner()
+        return_code = not runner.run(suite).wasSuccessful()
+        sys.exit(return_code)
 
