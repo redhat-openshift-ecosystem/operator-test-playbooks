7847bdd095aa6462fb68ec66cb592ebcffeabe0a
J0zi <jbreza@redhat.com>
2022-07-27 14:36:28 +0200

    FBC base image change (#363)
    * OSE base image for fbc (#362)
    
    Signed-off-by: J0zi <jbreza@redhat.com>

--- upstream/test/osr_test.sh
+++ upstream/test/osr_test.sh
@@ -30,4 +30,6 @@ ansible-playbook -i localhost, -e ansible_connection=local upstream/local.yml \
 -e automatic_cluster_version_label=true \
 -e image_protocol="docker://" \
 -e force_skip_mirror=false \
+# -e triggered_by_temp_index=true \
+# -e multiarch_tagged_fbc_is_needed=true \
 $OP_ANSIBLE_EXTRA
