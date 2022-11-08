94ab8868677bb88d6b7cfaf6e7b5b9cf737967ba
Martin Vala <mavala@redhat.com>
2022-11-08 20:33:38 +0100

    Fix
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- filter_plugins/sort_versions.py
+++ filter_plugins/sort_versions.py
@@ -1,11 +1,24 @@
-from packaging.version import Version
+# from packaging.version import Version
+# def version_sort(l):
+#     return sorted(l, key=Version)
+# class FilterModule(object):
+#     def filters(self):
+#         return {
+#             'sort_versions': version_sort
+#         }
 
-def version_sort(l):
-    return sorted(l, key=Version)
+from pkg_resources import parse_version
+
+def filter_sort_versions(value):
+    list = [item + '-z' for item in value]
+    list2 = sorted(list, key=parse_version)
+    return [x[:-2] for x in list2]
 
 class FilterModule(object):
+    filter_sort = {
+        'sort_versions': filter_sort_versions,
+    }
 
     def filters(self):
-        return {
-            'sort_versions': version_sort
-        }
+        return self.filter_sort
+
