bd0b2b0a17c0c237594b2883056d63b7f01c45da
Martin Vala <73172904+mvalarh@users.noreply.github.com>
2022-11-10 12:21:52 +0100

    [CVP-3380] Improve sort_version filter implementation (#410)
    * Improved sort_versions
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Remove   sort_versions  when not needed
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Cleanup
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Remove tests
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- filter_plugins/sort_versions.py
+++ filter_plugins/sort_versions.py
@@ -1,24 +1,9 @@
-# from packaging.version import Version
-# def version_sort(l):
-#     return sorted(l, key=Version)
-# class FilterModule(object):
-#     def filters(self):
-#         return {
-#             'sort_versions': version_sort
-#         }
-
-from pkg_resources import parse_version
-
-def filter_sort_versions(value):
-    list = [item + '-z' for item in value]
-    list2 = sorted(list, key=parse_version)
-    return [x[:-2] for x in list2]
-
+from packaging.version import Version
+def filter_sort_versions(l):
+    ls = [str(x) for x in l]
+    return sorted(ls, key=Version)
 class FilterModule(object):
-    filter_sort = {
-        'sort_versions': filter_sort_versions,
-    }
-
     def filters(self):
-        return self.filter_sort
-
+        return {
+            'sort_versions': filter_sort_versions
+        }
